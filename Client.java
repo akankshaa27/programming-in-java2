a2d5153840221d55ca7913084d5fe2a63928f8f6
akankshaa27 <soodakanksha0@gmail.com>
2024-04-26 13:39:54 +0530

    file added to new1

---
+++ Client.java
@@ -0,0 +1,12 @@
+package com.helloworlddemo;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//print Hello World
+		System.out.println("Hello World!!!");
+
+	}
+}
